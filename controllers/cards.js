8b09d76076040792fe1d199bd9498ff1cf664af8
Yegor Sidorenko <ye.sidorenko@gmail.com>
2023-05-22 16:19:54 +0300

    Project 13.1.4

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,9 +12,8 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const owner = req.user._id;
   cardSchema
-    .create({ name, link, owner })
+    .create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
