54cf2b8a13c6ad0ee30d177d2f2dd4d5ceea1e16
Yegor Sidorenko <ye.sidorenko@gmail.com>
2023-05-29 10:50:15 +0300

    Project 14.1.3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,16 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UserAuthError = require('../errors/UserAuthError');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UserAuthError('Unauthorized');
-  }
+  const token = req.cookies.jwt;
 
   let payload;
-  const token = authorization.replace('Bearer ', '');
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new UserAuthError('Unauthorized');
+    return next(new UserAuthError('Unauthorized'));
   }
 
   req.user = payload;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const DataNotFoundError = require('../errors/DataNotFoundError');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('/*', (req, res) => {
-  res.status(404).send({ message: 'Error 404: page is not found' });
+router.use('/*', (req, res, next) => {
+  next(new DataNotFoundError('Not found'));
 });
 
 module.exports = router;
