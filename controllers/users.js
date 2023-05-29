f599a1b21d225da34947b564330f001497ad3c53
Yegor Sidorenko <ye.sidorenko@gmail.com>
2023-05-29 10:58:56 +0300

    come back

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -108,7 +108,7 @@ module.exports.login = (req, res, next) => {
           if (!isValidPassword) {
             return Promise.reject(new UserAuthError('Invalid email or password'));
           }
-          const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
           res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true });
           return res.status(200).send({ token });
         });
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,19 @@
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UserAuthError = require('../errors/UserAuthError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UserAuthError('Unauthorized');
+  }
 
   let payload;
 
+  const token = authorization.replace('Bearer ', '');
+
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new UserAuthError('Unauthorized'));
   }
